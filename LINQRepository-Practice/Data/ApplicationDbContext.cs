462622a307f814214c1d3f42a230b58fa3fdae1b
jun.calupcupan <jun.calupcupan@DESKTOP-KUGSJMJ>
2024-07-03 08:41:44 +1200

    Add dbContext

---
+++ LINQRepository-Practice/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using LINQRepository_Practice.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LINQRepository_Practice.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<tblCustomer> tblCustomer { get; set; }
+    }
+}
