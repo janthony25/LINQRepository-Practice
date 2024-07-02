00e8130fc72f8d61fe59b9f975938bdca75e5517
jun.calupcupan <jun.calupcupan@DESKTOP-KUGSJMJ>
2024-07-03 08:35:41 +1200

    Add tblCustomerModel

---
+++ LINQRepository-Practice/Models/tblCustomer.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LINQRepository_Practice.Models
+{
+    public class tblCustomer
+    {
+        [Key]
+        public string CarRego { get; set; }
+        [Required]
+        public string CustomerName { get; set; }
+        public string? CustomerEmail { get; set; }
+        public string? CarMake { get; set; }
+        public string? CarModel { get; set; }
+        public string? PaymentStatus { get; set; }
+    }
+}
